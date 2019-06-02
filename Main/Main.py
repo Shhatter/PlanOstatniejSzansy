53bf5345a6f0131df9db72bb9144677e5b8aec56
Shhatter <bartez378@gmail.com>
2019-06-02 17:14:19 +0200

    data

--- Main/Main.py
+++ Main/Main.py
@@ -1,15 +1,22 @@
 import sys
-
+import random
 from PyQt5.QtCore import pyqtSlot
 from PyQt5.QtWidgets import QApplication, QDialog
 from PyQt5.uic import loadUi
 
-
+gFCompliments = [
+    "Najfajniejszy Kruszek na świecie",
+    "Najmądrzejszy Kruszek",
+    "Najlepiej zdający egzaminy Kruszek"
+]
 class MainDialog(QDialog):
     def __init__(self):
         super(MainDialog, self).__init__()
         loadUi('main.ui', self)
-        self.setWindowTitle('raz dwa trzy')
+        self.questionNumber = 0
+        self.slideNumber = 0
+
+        self.setWindowTitle(gFCompliments[ random.randint(0,len(gFCompliments)-1)])
 
 
 app = QApplication(sys.argv)
